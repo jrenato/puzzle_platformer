5fc65078f6845f7ccbdbc28f066c750a8b2a5fd9
karvalho <renato@karvalho.com.br>
2024-04-04 16:09:21 -0300

    Improved jump animation

--- Player/player.gd
+++ Player/player.gd
@@ -5,13 +5,19 @@ extends CharacterBody2D
 @export var movement_speed: int = 300
 @export var gravity: int = 30
 @export var jump_strength: int = 600
+@export var max_jumps: int = 1
 
 @onready var initial_sprite_scale: Vector2 = player_sprite.scale
 
+var jump_count: int = 0
 
-func _physics_process(delta):
+func _ready() -> void:
+	player_sprite.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
+
+
+func _physics_process(delta: float):
 	# Get player input
-	var horizontal_input = (
+	var horizontal_input: float = (
 		Input.get_action_strength("move_right")
 		- Input.get_action_strength("move_left")
 	)
@@ -23,13 +29,23 @@ func _physics_process(delta):
 	# Check player state
 	var is_falling: bool = velocity.y > 0.0 and not is_on_floor()
 	var is_jumping: bool = Input.is_action_just_pressed("jump") and is_on_floor()
+	var is_double_jumping: bool = Input.is_action_just_pressed("jump") and is_falling
 	var is_jumping_cancelled: bool = Input.is_action_just_released("jump") and velocity.y < 0.0
 	var is_idle: bool = is_on_floor() and is_zero_approx(horizontal_input) 
 	var is_walking: bool = is_on_floor() and not is_zero_approx(horizontal_input)
 
 
 	if is_jumping:
+		jump_count += 1
 		velocity.y = -jump_strength
+	elif is_double_jumping:
+		jump_count += 1
+		if jump_count <= max_jumps:
+			velocity.y = -jump_strength
+	elif is_jumping_cancelled:
+		velocity.y = 0.0
+	elif is_on_floor():
+		jump_count = 0
 
 	# Apply movement
 	move_and_slide()
@@ -38,6 +54,8 @@ func _physics_process(delta):
 	# Update player sprite animation
 	if is_jumping:
 		player_sprite.play("jump_start")
+	elif is_double_jumping:
+		player_sprite.play("double_jump_start")
 	elif is_walking:
 		player_sprite.play("walk")
 	elif is_falling:
@@ -53,3 +71,7 @@ func _physics_process(delta):
 
 		if horizontal_input > 0:
 			player_sprite.scale = initial_sprite_scale
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	player_sprite.play("jump")
