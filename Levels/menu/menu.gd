08bc7bd7de6d45e7ad0ae4e867c15a347f37d96d
karvalho <renato@karvalho.com.br>
2024-04-15 22:57:13 -0300

    Starting game

--- Levels/menu/menu.gd
+++ Levels/menu/menu.gd
@@ -1,35 +1,57 @@
-extends Control
+extends Node
 
+@export var ui: Control
+@export var level_container: Node
+@export var level_scene: PackedScene
 @export var ip_line_edit: LineEdit
 @export var status_label: Label
+@export var not_connected_hbox: HBoxContainer
+@export var host_hbox: HBoxContainer
 
 
 func _ready() -> void:
 	multiplayer.connection_failed.connect(_on_connection_failed)
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
-
-func _process(delta: float) -> void:
-	pass
+	not_connected_hbox.show()
+	host_hbox.hide()
 
 
 func _on_host_button_pressed() -> void:
+	not_connected_hbox.hide()
+	host_hbox.show()
 	Lobby.create_game()
+	status_label.text = "Hosting!"
 
 
 func _on_join_button_pressed() -> void:
+	not_connected_hbox.hide()
 	Lobby.join_game(ip_line_edit.text)
-
 	status_label.text = "Connecting..."
 
 
 func _on_start_button_pressed() -> void:
-	pass
+	hide_menu.rpc()
+	change_level.call_deferred(level_scene)
+
+
+func change_level(scene: PackedScene) -> void:
+	for child in level_container.get_children():
+		level_container.remove_child(child)
+		child.queue_free()
+
+	level_container.add_child(scene.instantiate())
 
 
 func _on_connection_failed() -> void:
-	status_label.text = "Connection failed"
+	status_label.text = "Connection failed!"
+	not_connected_hbox.show()
 
 
 func _on_connected_to_server() -> void:
-	status_label.text = "Connected"
+	status_label.text = "Connected!"
+
+
+@rpc("call_local", "authority", "reliable")
+func hide_menu() -> void:
+	ui.hide()
